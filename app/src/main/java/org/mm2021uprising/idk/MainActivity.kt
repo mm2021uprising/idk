bd070253537bf8159bb60323d806a86cbaf4b4dd
aunkhtooaung <aunkhtooaung@nexlabs.co>
2021-03-07 18:34:48 +0630

    feat: added checking device admin active or not

--- app/src/main/java/org/mm2021uprising/idk/MainActivity.kt
+++ app/src/main/java/org/mm2021uprising/idk/MainActivity.kt
@@ -1,11 +1,67 @@
 package org.mm2021uprising.idk
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import org.mm2021uprising.idk.util.Constants
 
 class MainActivity : AppCompatActivity() {
+
+  private val dpm: DevicePolicyManager by lazy {
+    getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+  }
+
+  private val deviceAdminPromptDialog: AlertDialog by lazy {
+    val builder = AlertDialog.Builder(this)
+    builder.setCancelable(false)
+    builder.create()
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+    //init device policy manager
+
+  }
+
+  private fun proceedDeviceAdministration() {
+    val isActiveDeviceAdmin = dpm.isAdminActive(
+        ComponentName(Constants.IDK_DEVICE_ADMIN_PACKAGE, Constants.IDK_DEVICE_ADMIN_CLASS)
+    )
+
+    if (!isActiveDeviceAdmin) {
+      //enable device admin first
+      AlertDialog.Builder(this@MainActivity)
+          .setTitle(R.string.notice)
+          .setMessage(R.string.device_admin_notice_msg)
+          .setCancelable(false)
+          .setPositiveButton(R.string.ok) { dialog, _ ->
+            dialog.dismiss()
+            //open device admin setting
+            val deviceAdminIntent = Intent()
+
+            deviceAdminIntent.component =
+              ComponentName("com.android.settings", "com.android.settings.DeviceAdminSettings")
+
+            startActivity(deviceAdminIntent)
+          }
+          .setNegativeButton(R.string.cancel) { dialog, _ ->
+            dialog.dismiss()
+            finishAffinity()
+          }
+          .create()
+          .also { deviceAdminProceedDialog ->
+          }
+          .show()
+    }
+  }
+
+  override fun onResume() {
+    super.onResume()
+    proceedDeviceAdministration()
   }
 }
---
+++ app/src/main/java/org/mm2021uprising/idk/util/Constants.kt
@@ -0,0 +1,6 @@
+package org.mm2021uprising.idk.util
+
+object Constants {
+  const val IDK_DEVICE_ADMIN_PACKAGE = "org.mm2021uprising.idk"
+  const val IDK_DEVICE_ADMIN_CLASS = "org.mm2021uprising.idk.IDKDeviceAdminReceiver"
+}
